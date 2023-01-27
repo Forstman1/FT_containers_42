9c32bcd392e0f273402a527accd21334ad22991d
Sami Hafid <sahafid@e2r5p15.1337.ma>
2023-01-27 23:53:23 +0100

    finish most of member functions for vector that doesn't contain iterator

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: sahafid <sahafid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 18:22:56 by sahafid           #+#    #+#             */
-/*   Updated: 2023/01/27 19:06:45 by sahafid          ###   ########.fr       */
+/*   Updated: 2023/01/27 23:49:52 by sahafid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,12 @@ struct test {
 int main()
 {
     const test var(123);
-    ft::vector<test> vec;
-    vec.push_back(var);
+    // ft::vector<test> vec;
+    // vec.push_back(var);
     ft::vector<int> ob;
     std::vector<int> vect;
+
+
 
     ob.push_back(5);
     ob.push_back(10);
@@ -39,14 +41,46 @@ int main()
     ob.pop_back();
     ob.push_back(12);
 
-    
     vect.push_back(5);
     vect.push_back(10);
     vect.push_back(15);
     vect.pop_back();
     vect.push_back(12);
     
-    ob.print(2);
-    std::cout << vect[2] << std::endl;
+
+    ob.resize(10, 10);
+    vect.resize(10, 10);
+    
+    std::cout << ob.capacity() << std::endl;
+    std::cout << vect.capacity() << std::endl;
+
+    // vect.pop_back();
+    // ob.pop_back();
+
+
+    vect.reserve(11);
+    ob.reserve(11);
+    
+    std::cout << ob.size() << std::endl;
+    std::cout << vect.size() << std::endl;
+    
+    std::cout << ob.capacity() << std::endl;
+    std::cout << vect.capacity() << std::endl;
+
+    std::cout << ob.front() << std::endl;
+    std::cout << vect.front() << std::endl;
+
+    std::cout << ob.data() << std::endl;
+    std::cout << vect.data() << std::endl;
+
+    // std::cout << ob[2] << std::endl;
+    // std::cout << vect << std::endl;
+    ob.clear();
+    vect.clear();
+
+    std::cout << vect.capacity() << std::endl;
+    std::cout << ob.capacity() << std::endl;
+
+    
     return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,70 +6,230 @@
 /*   By: sahafid <sahafid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 18:23:59 by sahafid           #+#    #+#             */
-/*   Updated: 2023/01/27 19:04:55 by sahafid          ###   ########.fr       */
+/*   Updated: 2023/01/27 23:49:06 by sahafid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <alloca.h>
 #include <memory>
-
+#include <exception>
 
 namespace ft {
 
-    template <typename T, typename Allocat = std::allocator<T> >
+    template <typename T, typename allocator_type = std::allocator<T> >
     class vector {
         private:
+
             T *array;
             size_t size_filled;
-            size_t size;
-            Allocat allocator;
+            size_t capacity_;
+            allocator_type allocator;
+            
         public:
+
+
+
+
+        
             vector(){
-                array = NULL;
-                size_filled = 0;
-                size = 0;
+                this->array = NULL;
+                this->size_filled = 0;
+                this->capacity_ = 0;
             }
-            void    print(int index) {
-                std::cout << array[index] << std::endl;
+            ~vector() {}
+
+            vector& operator= (const vector& x)
+            {
+                for (int i = 0; i < this->capacity(); i++)
+                    this->allocator.destroy(&array[i]);
+
+                    
+                this->allocator.deallocate(array, this->capacity());
+                
+                this->capacity_ = x.capacity();
+                this->size_filled = x.size();
+                
+                this->allocator.allocate(this->capacity);
+                
+                for (int i = 0; i < x.size(); i++)
+                    this->allocator.construct(&this->array[i], x.array[i]);
+                
+                return (*this);
             }
 
 
-            void    push_back(const T &value) {
-                if (size == 0)
+            
+            size_t    size() {
+                return (this->size_filled);
+            }
+
+
+            size_t    capacity() {
+                return (this->capacity_);
+            }
+
+
+            bool    empty() {
+
+                if (size_filled != 0)
+                    return (1);
+                return 0;
+            }
+
+
+            
+            void    resize(size_t n, T val = T()) {
+                
+                if (n < this->size())
                 {
-                    size++;
-                    size_filled++;
-                    array = allocator.allocate(1);
-                    allocator.construct(&array[0], value);
+                    for (int i = this->size(); i > n; i--)
+                        this->allocator.destroy(&this->array[i-1]);
+                }
+                else if (n > this->size() && n < capacity())
+                {
+                    for (int j = this->size(); j < n; j++)
+                        push_back(val);
                 }
                 else
                 {
-                    size_filled++;
-                    if (size_filled + 1 > size)
+                    for (int j = this->size(); j < n; j++)
                     {
-                        T *tmp = array;
-                        size_t tmp_size = size;
-                        size *= 2;
-                        array = allocator.allocate(size);
-                        for (int i = 0; i < size_filled-1; i++)
+                        if (j < this->capacity())
+                            push_back(val);
+                        else
                         {
-                            allocator.construct(&array[i], tmp[i]);
-                            allocator.destroy(&tmp[i]);
-                            
-                        }
-                        allocator.construct(&array[size_filled-1], value);
-                        allocator.deallocate(tmp, tmp_size);
-                    }
-                    else
-                        allocator.construct(&array[size_filled-1], value);
+                            T *tmp = this->array;
+                            capacity_++;
+                            this->size_filled++;    
+                            this->array = this->allocator.allocate(this->capacity_);
+                            for (int i = 0; i < this->size_filled-1; i++)
+                            {
+                                this->allocator.construct(&this->array[i], tmp[i]);
+                                this->allocator.destroy(&tmp[i]);
+                            }
+                            this->allocator.construct(&this->array[this->size_filled-1], val);
+                            this->allocator.deallocate(tmp, capacity() -1);
+                        }        
+                    }   
                 }
             }
-            void    pop_back()
-            {
-                allocator.destroy(&array[size_filled-1]);
-                size_filled--;
+
+
+
+            void    reserve(size_t n) {
+                
+                if (n > this->capacity())
+                {
+                    for (int j = this->capacity(); j < n; j++)
+                    {
+                        T *tmp = this->array;
+                        capacity_++; 
+                        this->array = this->allocator.allocate(this->capacity_);
+                        for (int i = 0; i < this->size_filled-1; i++)
+                        {
+                            this->allocator.construct(&this->array[i], tmp[i]);
+                            this->allocator.destroy(&tmp[i]);
+                        }
+                        this->allocator.deallocate(tmp, capacity() -1);
+                    }
+                }
             }
             
+            T& operator[] (size_t n)
+            {
+                return (array[n]);
+            }
+            const T& operator[] (size_t n) const
+            {
+                return (array[n]);
+            }
+            T& at(size_t n) {
+            
+                if (n >= this->size())
+                    throw std::out_of_range("vector");
+                return (array[n]);
+            }
+            
+            const T& at(size_t n) const
+            {
+                if (n >= this->size())
+                    throw std::out_of_range("vector");
+                return (array[n]);
+            }
+
+            T& front() {
+                return (array[0]);
+            }
+            const T& front() const {
+                return (array[0]);
+            }
+            
+             T& back() {
+                return (array[this->size() - 1]);
+            }
+            const T& back() const {
+                return (array[this->size() - 1]);
+            }
+
+            T* data() {
+                return (array);
+            }
+            const T* data() const 
+            {
+                return (array);
+            }
+            
+            void    push_back(const T &value) {
+                
+                if (capacity_ == 0)
+                {
+                    this->capacity_++;
+                    this->size_filled++;
+                    this->array = this->allocator.allocate(1);
+                    this->allocator.construct(&this->array[0], value);
+                }
+                else
+                {
+                    this->size_filled++;
+                    if (this->size_filled > this->capacity_)
+                    {
+                        T *tmp = this->array;
+                        size_t tmp_size = this->capacity_;
+                        this->capacity_ *= 2;
+                        this->array = this->allocator.allocate(this->capacity_);
+                        for (int i = 0; i < this->size_filled-1; i++)
+                        {
+                            this->allocator.construct(&this->array[i], tmp[i]);
+                            this->allocator.destroy(&tmp[i]);
+                        }
+                        this->allocator.construct(&this->array[this->size_filled-1], value);
+                        this->allocator.deallocate(tmp, tmp_size);
+                    }
+                    else
+                        this->allocator.construct(&this->array[this->size_filled-1], value);
+                }
+            }
+
+            
+            void    pop_back() {
+                
+                if (this->capacity_ == 0 || this->size_filled == 0)
+                    return ;
+                this->allocator.destroy(&this->array[this->size_filled-1]);
+                this->size_filled--;
+            }
+
+            void    clear()
+            {
+                for (int i = 0; i < this->size_filled; i++)
+                    this->allocator.destroy(&array[i]);
+                size_filled = 0;
+            }
+
+            void    swap(vector &x)
+            {
+                
+            }
     };
 }
